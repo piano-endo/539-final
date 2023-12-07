2bad53ee479d1f53bee3201e6679d45235f4d0b7
piano-endo <143748197+piano-endo@users.noreply.github.com>
2023-12-06 19:44:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
-const curDate = new Date();
-let curYear = curDate.getFullYear();
-let curMonth = curDate.getMonth() + 1;
-let maxYear = curYear + 1;
-let customerName;
-
-document.addEventListener("DOMContentLoaded", () => {
-  const calendar = new VanillaCalendar("#calendar", {
-    settings: {
-      range: {
-        min: curYear + "-" + curMonth + "-" + "01",
-        max: maxYear + "-" + curMonth + "-" + "01",
-      },
-    },
-    popups: {
-      "2023-12-03": {
-        html: `<div>
-            <p style="background-color: rgb(179 195 189)">We close at 3PM</p>
-          </div>`,
-      },
-      "2023-12-17": {
-        html: `<div>
-            <p style="background-color: rgb(179 195 189)">We close at 3PM</p>
-          </div>`,
-      },
-      "2023-12-24": {
-        html: `<div>
-            <p style="background-color: rgb(179 195 189)">We are closed for Christmas preparation</p>
-          </div>`,
-      },
-      "2023-12-25": {
-        html: `<div>
-            <p style="background-color: rgb(179 195 189)">We have Stollen and decorations ready for Christmas</p>
-          </div>`,
-      },
-    },
-  });
-  calendar.init();
-});
-
-function getName() {
-  customerName = document.getElementById("customerName").value;
-  document.getElementById("result").innerHTML = "hi " + customerName;
-  let pickUpTime = document.getElementById("orderH2");
-  let buttonSoon = document.getElementById("button_soon");
-  let buttonLater = document.getElementById("button_later");
-  pickUpTime.style.display = "block";
-  buttonSoon.style.display = "block";
-  buttonLater.style.display = "block";
+function home() {
+  console.log("test");
+  let currentRoot = window.location.protocol + "//" + window.location.host;
+  let homePage = currentRoot + "/index.html";
+  window.open(homePage, "_self");
 }
+
+function greeting() {
+  let name = sessionStorage.getItem("customerName");
+  console.log(name);
+  document.getElementsByClassName("greeting")[0].innerHTML = "Hi " + name;
+}
+
+function thanks() {
+  let name = sessionStorage.getItem("customerName");
+  let order = sessionStorage.getItem("Order");
+  console.log(order);
+  document.getElementsByClassName("thanks")[0].innerHTML = "Thank you " + name;
+  document.getElementsByClassName("order")[0].innerHTML =
+    "Your order is confirmed: " + order;
+}
+
+function saveOrder() {
+  let orderedItem =
+    document.getElementsByClassName("ordered-item")[0].innerHTML;
+  sessionStorage.setItem("Order", orderedItem);
+}
+
+// document.addEventListener("onload", greeting());
+// document
+//   .getElementsByClassName("logo")[0]
+//   .addEventListener("click", function () {
+//     let currentRoot = window.location.protocol + "//" + window.location.host;
+//     let homePage = currentRoot + "/index.html";
+//     console.log(homePage);
+//     window.open(homePage, "_self");
+//   });
